73d089ca6f4c4a34d37d5f003bedfa4a2015d9a8
Griffin T <griffin_git@thommo.io>
2024-04-11 16:22:11 +0800

    Setup basic error handling and logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use color_eyre::Result;
+
+fn main() -> Result<()> {
+    setup()?;
+
+    Ok(())
+}
+
+fn setup() -> Result<()> {
+    color_eyre::install()?;
+
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+    tracing_subscriber::fmt::init();
+
+    Ok(())
 }
